42c95788d1cfbe19ca785465563ab481628b944f
Akshay <akshaya@inuxu.media>
2026-03-12 18:18:50 +0530

    Added transitivity tracking field in audit log

--- spark/jobs/cluster/entity_resolution_job.py
+++ spark/jobs/cluster/entity_resolution_job.py
@@ -1,30 +1,30 @@
+import os
+import sys
+# This must be first line before any hotel data import
+sys.path.append('/opt/airflow')
+from hotel_data.config.paths import (
+    BASE_DELTA_PATH,
+    CATALOG_NAME,
+    SCHEMA_NAME,
+    TABLE_HOTELS_NAME,
+    TABLE_HOTELS_PAIRS_NAME
+)
+from hotel_data.delta.delta_table_manager import DeltaTableManager
+import hotel_data.pipeline.scoring.scorers.overall_pair_scorer as overall_pair_scorer
+from spark.jobs.cluster.entity_resolution_pipeline import EntityResolutionPipeline
+from pyspark.sql import Window
+from pyspark.sql import functions as F
+from pyspark.sql import SparkSession, Window
 import json
 import os
 import sys
 import functools
 # Ensure we can import custom modules (Standard Airflow setup)
-sys.path.append('/opt/airflow')
-from pyspark.sql import SparkSession, Window
 # import pyspark.sql.functions as F
-from pyspark.sql import functions as F
-from pyspark.sql import Window
 
-from spark.jobs.cluster.entity_resolution_pipeline import EntityResolutionPipeline
-
-
-
-from hotel_data.config.paths import (
-    BASE_DELTA_PATH, 
-    CATALOG_NAME, 
-    SCHEMA_NAME, 
-    TABLE_HOTELS_NAME, 
-    TABLE_HOTELS_PAIRS_NAME
-)
-from hotel_data.delta.delta_table_manager import DeltaTableManager
 
 # We assume you have or will define a constant for the final clusters table
-TABLE_FINAL_CLUSTERS_NAME = "final_clusters" 
-
+TABLE_FINAL_CLUSTERS_NAME = "final_clusters"
 
 
 def main():
@@ -32,7 +32,7 @@ def main():
     spark.sparkContext.setLogLevel("WARN")
 
     # These would typically come from Airflow DAG parameters
-    current_provider = os.getenv('PROVIDER_NAME', 'hbose') 
+    current_provider = os.getenv('PROVIDER_NAME', 'hbose')
     print(f"🔎 EntityResolution provider: {current_provider}")
     TABLE_HISTORY_NAME = "comparison_audit_log"
     transitivity = os.getenv("TRANSITIVITY", "true").lower() == "true"
@@ -43,20 +43,26 @@ def main():
         't_low': float(os.getenv('THRESHOLD_LOW', 0.80)),
         'transitivity': transitivity,
     }
-    
-    match_logic = json.loads(os.getenv('MATCH_LOGIC', '{}'))
 
-    manager = DeltaTableManager(spark, CATALOG_NAME, SCHEMA_NAME, BASE_DELTA_PATH)
+    match_logic = json.loads(os.getenv('MATCH_LOGIC', '{}'))
+    print(f"⚙️ Loaded match logic1 : {json.dumps(match_logic, indent=2)}")
+
+    match_logic = overall_pair_scorer._load_match_logic()
+    print(f"⚙️ Loaded match logic2 : {json.dumps(match_logic, indent=2)}")
+
+    manager = DeltaTableManager(
+        spark, CATALOG_NAME, SCHEMA_NAME, BASE_DELTA_PATH)
     pipeline = EntityResolutionPipeline(spark, config, match_logic)
-    
+
     pipeline.run(
-        manager=manager, 
-        table_hotels=TABLE_HOTELS_NAME, 
-        table_pairs=TABLE_HOTELS_PAIRS_NAME, 
+        manager=manager,
+        table_hotels=TABLE_HOTELS_NAME,
+        table_pairs=TABLE_HOTELS_PAIRS_NAME,
         table_output=TABLE_FINAL_CLUSTERS_NAME,
         table_history=TABLE_HISTORY_NAME,
         current_provider=current_provider
     )
+
 
 if __name__ == "__main__":
     main()
